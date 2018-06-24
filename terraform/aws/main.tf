80d9dcf66abb7198b4829532567160f8a8af1ef9
Ruan Arcega <ruanarcega@gmail.com>
2018-06-24 20:29:40 -0300

    Add route53 tf configuration

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -14,3 +14,8 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "k8s-ruan-us-east-1"
   acl = "private"
 }
+
+# Create DNS Zone
+resource "aws_route53_zone" "ruan" {
+  name = "k8s-ruan-us-east-1.domain.io"
+}
